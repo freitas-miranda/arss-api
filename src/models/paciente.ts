b2c1f88dc285687c4bd6314840fcaddb4587f483
Alan Miranda <ti040@irmaosgoncalves.com.br>
2021-01-16 09:33:57 -0400

    Adiciona model Paciente

---
+++ src/models/paciente.ts
@@ -0,0 +1,42 @@
+import Pessoa from "@models/usuario";
+
+import {
+  AllowNull,
+  AutoIncrement,
+  Column,
+  DataType,
+  ForeignKey,
+  Model,
+  PrimaryKey,
+  Table
+} from "sequelize-typescript";
+
+@Table({ tableName: "paciente" })
+class Paciente extends Model<Paciente> {
+
+  @AutoIncrement
+  @PrimaryKey
+  @Column
+  id: number;
+
+  @ForeignKey(() => Pessoa)
+  @AllowNull(false)
+  @Column
+  pessoaId: number;
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  cartaoSus: string;
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  tipoSanguineo: string;
+
+  @Column(DataType.FLOAT)
+  peso: number;
+
+  @Column(DataType.FLOAT)
+  altura: number;
+}
+
+export default Paciente;
